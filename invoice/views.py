dec3a6266279e23f011cbcb158b52de59de0121a
pabarragang <pabarragang@outlook.es>
2017-07-15 12:09:02 -0500

    CRUD product

--- invoice/views.py
+++ invoice/views.py
@@ -19,10 +19,61 @@ def createProduct(request):
 					iva = request.POST['iva']
 				product = Product(name=request.POST['name'],packing=request.POST['packing'],price=request.POST['price'],iva=iva)
 				product.save()
-				response['response'] = "Creado"
+				response['response'] = "Creted"
 			response['response'] = "Without price"
 		response['response'] = "Without packing"
 	response['response'] = "Without name"
 	return HttpResponse(json.dumps(response))
 
+def removeProduct(request):
+	response = {}
+	if request.POST['id']:
+		product = Product.objects.get(id=request.POST['id'])
+		if product:
+			product.delete()
+			response['response'] = "Deleted"
+		else:
+			response['response'] = "This product does nor exist"
+	else:
+		response['response'] = "Without id"
+	return HttpResponse(json.dumps(response))
+
+def modifiedProduct(request):
+	response = {}
+	if request.POST['id']:
+		product = Product.objects.get(id=request.POST['id'])
+		if product:
+			if request.POST['name']:
+				product.name = request.POST['name']
+			if request.POST['packing']:
+				product.packing = request.POST['packing']
+			if request.POST['price']:
+				product.price = request.POST['price']
+			if request.POST['iva']:
+				product.iva = request.POST['iva']
+			product.save()
+			response['response'] = "Modified"
+		else:
+			response['response'] = "This product does not exist"
+		return HttpResponse(json.dumps(response))
+	else:
+		response['response'] = "Without id"
+		return HttpResponse(json.dumps(response))
+
+def getProduct(request):
+	response = {}
+	if request.POST['id']:
+		product = Product.objects.get(id=request.POST['id'])
+		if product:
+			response['name']=product.name
+			response['packing']=product.packing
+			response['price']=product.price
+			response['iva']=product.iva
+			return HttpResponse(json.dumps(response))
+		else:
+			response['response'] = "This product does not exist"
+			return HttpResponse(json.dumps(response))
+	else:
+		response['response'] = "Without id"
+		return HttpResponse(json.dumps(response))
 
